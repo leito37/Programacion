f0c9282403800cfa9507292f41173528c8f88ab8
2022160045 <133302244+2022160045@users.noreply.github.com>
2024-04-28 10:25:47 -0500

    actualizacion

--- daniel.php
+++ daniel.php
@@ -26,6 +26,36 @@ class toyota extends vehiculoTerrestre{
         echo 'el toyota acelera a ' . $string . PHP_EOL;
     }
 }
-$carro=new toyota;
-$carro->printAcelerar("40km/s")
+$carro = new toyota;
+$carro->printAcelerar("40km/s");
+
+//3
+class fruta{
+    public function printColor($string){
+        echo 'color' . $string . PHP_EOL;
+    }
+}
+class manzana extends fruta{
+    public function printColor($string){
+        echo 'rojo' . $string . PHP_EOL;
+    }
+}
+$manzIsrael = new fruta;
+$manzIsrael->printColor("-");
+
+//4
+class instMusical{
+    public function printSonido($string){
+        echo 'sonido de ' . $string . PHP_EOL;
+    }
+}
+class instPercusion extends instMusical{
+    public function printSonido($string)
+    {
+        parent::printSonido($string);
+        echo ' es de percucion ' . PHP_EOL;
+    }
+}
+$cajon = new instPercusion;
+$cajon->printSonido("cajón");
 ?>
