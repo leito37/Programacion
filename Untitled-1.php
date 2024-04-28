55a815b98dd470c7e1739666e54b650cd9a2b4e3
leito37 <168307918+leito37@users.noreply.github.com>
2024-04-27 23:03:57 -0500

    uno

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,5 +1,28 @@
 <?php
+    
+    class Educacion
+    {
+        private function printItem($string)
+        {
+            echo 'Educar: ' . $string . PHP_EOL;
+        }
 
-    echo "Hola mundo";
+        private function printPHP()
+        {
+            echo 'PHP is great.' . PHP_EOL;
+        }
+    }
+
+    class Inteligencia extends Educacion
+    {
+        private function printItem($string)
+        {
+            parent::printItem($string);
+            echo 'Inteligencia->: ' . $string . PHP_EOL;
+        }
+    }
+
+    $bunyar = new Inteligencia();
+    $bunyar->printItem("Hello");
 
 ?>
